c485b0258bda897c192f5a92ebea718e7734f09e
kalugin-stepan <kalugin.stepan.dev@gmail.com>
2021-11-11 11:38:26 +0300

    controller

--- configure_mysql.js
+++ configure_mysql.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 
-const fs = require("fs");
-const DataBase = require("./dist/database.js").DataBase;
-DataBase.configure(JSON.parse(fs.readFileSync("./mysql_config.json")));
+const fs = require("fs")
+const DataBase = require("./dist/database.js").DataBase
+DataBase.configure(JSON.parse(fs.readFileSync("./mysql_config.json")))
